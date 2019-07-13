929b2cb874d6c4f2bb487b160fb413865731d03c
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-13 10:20:27 +1000

    Add FontAwesomeIcon

--- src/infoPage/info.js
+++ src/infoPage/info.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCarSide, faTemperatureHigh, faShower, faTree } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faCarSide, faTemperatureHigh, faShower, faTree);
 
 const Info = () =>{
   return(
     <div>
       <h1>Milk (dairy)</h1>
       <h2>1 glass (200ml) per serving</h2>
+
+
       <p>Over an entire year your consumption of dairy milk is contributing 229kg to your annual greenhouse gas emissions.</p>
-      <p>That's the equivalent of driving a regular petrol car 585 miles (941km).</p>
+      <p><FontAwesomeIcon icon={faCarSide} /> That's the equivalent of driving a regular petrol car 585 miles (941km).</p>
       <p>OR</p>
-      <p>the same as heating the average UK home for 36 days.</p>
+      <p><FontAwesomeIcon icon={faTemperatureHigh} /> the same as heating the average UK home for 36 days.</p>
       <p>Your consumption of dairy milk also uses</p>
-      <p>45,733 litres of water, equal to 703 showers lasting eight minutes.</p>
+      <p><FontAwesomeIcon icon={faShower} /> 45,733 litres of water, equal to 703 showers lasting eight minutes.</p>
       <p>AND</p>
-      <p>652m² land, equal to the space of 2 tennis courts.</p>
+      <p><FontAwesomeIcon icon={faTree} /> 652m² land, equal to the space of 2 tennis courts.</p>
     </div>
   )
 };
